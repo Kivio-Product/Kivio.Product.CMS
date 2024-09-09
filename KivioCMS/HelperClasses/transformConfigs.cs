0f60c8c9db354e5809ede1b30ac147b3d3ee74cf
sebasKivio <sebastian@kivio.com.co>
2024-09-09 16:38:31 -0500

    Configs update

--- KivioCMS/HelperClasses/transformConfigs.cs
+++ KivioCMS/HelperClasses/transformConfigs.cs
@@ -2,31 +2,34 @@ using Umbraco.Cms.Core.Models.PublishedContent;
 using Umbraco.Cms.Web.Common.PublishedModels;
 using Umbraco.Extensions;
 
-public class ConfigurationComponentHelper
+
+namespace KivioCMS.HelperClasses
 {
-    private readonly IPublishedValueFallback _publishedValueFallback;
+	public class transformConfigs
+	{
+		private IPublishedValueFallback _publishedValueFallback;
 
-    // Constructor to inject IPublishedValueFallback dependency
-    public ConfigurationComponentHelper(IPublishedValueFallback publishedValueFallback)
-    {
-        _publishedValueFallback = publishedValueFallback;
-    }
+		public transformConfigs(IPublishedValueFallback publishedValueFallback)
+		{
+			_publishedValueFallback = publishedValueFallback;
+		}
 
-    // Method to cast any IPublishedElement to ConfigurationsComponent
-    public ConfigurationsComponent CastToConfigurationsComponent(IPublishedElement element)
-    {
-        // Ensure the element is not null and is of the correct type
-        if (element == null) 
-            return null;
+		// Method to cast any IPublishedElement to ConfigurationsComponent
+		public ConfigurationsComponent CastToConfigurationsComponent(IPublishedElement element)
+		{
+			// Ensure the element is not null and is of the correct type
+			if (element == null)
+				return null;
 
-        // Check if the element has the alias "configurationsComponent"
-        if (element.ContentType.Alias == ConfigurationsComponent.ModelTypeAlias)
-        {
-            // Cast the element to ConfigurationsComponent
-            return new ConfigurationsComponent(element, _publishedValueFallback);
-        }
+			// Check if the element has the alias "configurationsComponent"
+			if (element.ContentType.Alias == ConfigurationsComponent.ModelTypeAlias)
+			{
+				// Cast the element to ConfigurationsComponent
+				return new ConfigurationsComponent(element, _publishedValueFallback);
+			}
 
-        // Return null if the element cannot be cast
-        return null;
-    }
+			// Return null if the element cannot be cast
+			return null;
+		}
+	}
 }
